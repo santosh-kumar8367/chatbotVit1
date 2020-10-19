d5bec6c93aeb71f7a94bd8b4dedd31315ba76522
santosh-kumar8367 <61039032+santosh-kumar8367@users.noreply.github.com>
2020-10-19 15:51:08 +0530

    new

--- storytelling (1).py
+++ storytelling (1).py
@@ -4,8 +4,9 @@ from playsound import playsound
 from gtts import gTTS
 import os
 res="false"
+
+#This function plays random music
 def music():
-    """ This function plays random music """
     print("Enjoy listening the music ",emoji.emojize(":thumbs_up:"))
     L = ["388851__enviromaniac2__piano3.mp3"]
     playsound(random.choice(L))
@@ -14,9 +15,8 @@ def music():
     if(res=="quit"):
         return res
 
-
+#This function plays audio for desired story choosen
 def story_telling():
-    """This function plays audio of story choosen"""
     Wanted_Story = input("Choose one among these: Story for kids, Horror, Thriller, Moral and Humorous : ")
     Stories = {'Story for kids':'Story_for_kids.txt','Horror':'Horror_story.txt' ,'Thriller':'Triller.txt','Moral':'Moral_story.txt' ,'Humorous':'Humorous.txt' }
     if Wanted_Story in Stories:
@@ -34,3 +34,4 @@ def story_telling():
     res=input()
     if(res=="quit"):
         return res
+#code ends
