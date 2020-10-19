81fcc3de85f94b2e8a3056d7e16fd03e6660e087
santosh-kumar8367 <61039032+santosh-kumar8367@users.noreply.github.com>
2020-10-19 15:09:27 +0530

    new

--- calculations (1).py
+++ calculations (1).py
@@ -1,11 +1,11 @@
 
-from math import *
+from math import * #import math library
 res="false"
 def cal_exp():
     """ This function  evaluates an expression"""
     exp_num=input("Enter expression : ")
     try:
-        print("Result of",exp_num,"=",eval(exp_num))
+        print("Result of",exp_num,"=",eval(exp_num))  #eval functions an expression 
     except Exception as e:
         print(str(e))
     print("press enter to continue or enter quit to stop")
